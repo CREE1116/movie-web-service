af804a63b489960e3feea966359357548335d29a
leejongmin <60211689@mju.ac.kr>
2022-12-29 14:33:39 +0900

    basename 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import { useLocation } from "react-router-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/Movie/:id" component={Detail} />
         <Route path="/" component={Home} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
